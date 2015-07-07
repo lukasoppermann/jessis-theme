2ad11aba70e9f5f51ef2df22b49a0a6105c28fbe
Lukas Oppermann <lukas@vea.re>
2015-07-07 19:26:40 +0200

    init

---
+++ footer.php
@@ -0,0 +1,89 @@
+	<div class="footer section large-padding bg-dark">
+
+		<div class="footer-inner section-inner">
+
+
+			<?php if ( is_active_sidebar( 'footer-a' ) ) : ?>
+
+				<div class="column column-1 left">
+
+					<div class="widgets">
+
+						<?php dynamic_sidebar( 'footer-a' ); ?>
+
+					</div>
+
+				</div>
+
+			<?php endif; ?> <!-- /footer-a -->
+
+			<?php if ( is_active_sidebar( 'footer-b' ) ) : ?>
+
+				<div class="column column-2 left">
+
+					<div class="widgets">
+
+						<?php dynamic_sidebar( 'footer-b' ); ?>
+
+					</div> <!-- /widgets -->
+
+				</div>
+
+			<?php endif; ?> <!-- /footer-b -->
+
+			<?php if ( is_active_sidebar( 'footer-c' ) ) : ?>
+
+				<div class="column column-3 left">
+
+					<div class="widgets">
+
+						<?php dynamic_sidebar( 'footer-c' ); ?>
+
+					</div> <!-- /widgets -->
+
+				</div>
+
+			<?php endif; ?> <!-- /footer-c -->
+
+			<div class="clear"></div>
+
+		</div> <!-- /footer-inner -->
+
+	</div> <!-- /footer -->
+
+	<div class="credits section bg-dark no-padding">
+
+		<div class="credits-inner section-inner">
+
+			<p class="credits-left">
+
+				&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
+
+			</p>
+
+			<?php if ( is_active_sidebar( 'footer-menu' ) ) : ?>
+
+				<div class="right">
+
+					<div class="widgets">
+
+						<?php dynamic_sidebar( 'footer-menu' ); ?>
+
+					</div>
+
+				</div>
+
+			<?php endif; ?> <!-- /footer-full -->
+
+			<div class="clear"></div>
+
+		</div> <!-- /credits-inner -->
+
+	</div> <!-- /credits -->
+
+</div> <!-- /big-wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
